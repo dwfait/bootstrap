74573444a63d7d388c590fa9a3e293c6fc21b4a6
Dwain Faithfull <dwfaithfull@gmail.com>
2012-12-23 05:17:26 +0000

    Moved dotfiles into their own folder

--- dotfiles.sh
+++ dotfiles.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "${BASH_SOURCE}")"
 git pull
 copy_files() {
-  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+  rsync  --exclude ".DS_Store" -av ./dotfiles ~
 }
 if [ "$1" = -f ]
 then
